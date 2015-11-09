de67d53cc9f923f15479357e2e3d59ade44050af
Chris Alwin <chris@somethingbig.co.uk>
2015-11-09 17:46:23 +0000

    add style sheets

--- wp-content/themes/GPRDehler/index.php
+++ wp-content/themes/GPRDehler/index.php
@@ -1,14 +1,21 @@
-<?php get_template_part('templates/page', 'header'); ?>
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
+get_header(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
 
-<?php the_posts_navigation(); ?>
+
+<?php get_footer(); ?>
