79672619db2d1480e45ded296f6478e6783299d8
Chris Alwin <chris@somethingbig.co.uk>
2015-11-16 17:46:22 +0000

    home page close to being finished on mobile

--- wp-content/themes/GPRDehler/footer.php
+++ wp-content/themes/GPRDehler/footer.php
@@ -11,6 +11,47 @@
 ?>
 
 <footer>
+    <div class=diamonds></div>
+    <div class="contactUs">
+        <div class="footer-form contact-form">
+            <h3>Contact us</h3>
+            <form>
+                <input type="text" placeholder="fullname" name="fullName">
+                <input type="email" placeholder="email address" name="email">
+                <select>
+                    <option value="" disabled selected>Type of enqury</option>
+                    <option value="TEST">Test</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
+
+        </div>
+        <div class="footer-form newsletter-form">
+            <h3>Newsletter</h3>
+            <p>Lorum ipsum dolor sit consectertur adispising</p>
+            <form>
+                <input type="text" placeholder="fullname" name="fullName">
+                <input type="email" placeholder="email address" name="email">
+                <button type="submit">Submit</button>
+            </form>
+
+
+        </div>
+
+        <div class="clearfix"></div>
+        <div class="address">
+            London
+        </div>
+        <div class="address">
+            Sydney
+        </div>
+        <div class="address">
+            San Diego
+        </div>
+
+
+    </div>
+
 
 
 </footer><!-- .site-footer -->
--- wp-content/themes/GPRDehler/page.php
+++ wp-content/themes/GPRDehler/page.php
@@ -16,13 +16,108 @@ get_header(); ?>
 	?>
 
 	<div class="content">
-		<div class=diamonds><span>EXPERIENCE</span></div>
+
 
 		<div class="slider">
+			<div class=diamonds><span>EXPERIENCE</span></div>
+
 			<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/slider/silde-1.jpg"/>
 		</div>
-		<div class="container">
+		<div class="container box">
+			<h2>We are an <span>innovative international</span> management consulting company</h2>
 
+			<p> We are passionate about delivering sustainable and measurable client success, and know that to improve the way a business
+				works, you need to start with the people behind the processes. We’re experts at thinking in new ways and taking risks.
+				This means that when it comes to implementing organisational change, your people will be empowered to innovate
+				beyond what other businesses are doing and establish you at the forefront of your industry.</p>
+
+		</div>
+		<div class="box whoAreWe">
+
+			<ul>
+				<li>Who we are
+					<div class="imgWrap">
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/who-img1.png"/>
+
+
+					</div>
+				</li>
+				<li>Our approach
+					<div class="imgWrap">
+						<img style="top: -50px;" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/who-img2.png"/>
+
+
+					</div></li>
+				<li>Industries
+					<div class="imgWrap">
+						<img style="top: -85px;" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/who-img3.png"/>
+
+
+					</div></li>
+				<li>Articles
+					<div class="imgWrap">
+						<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/who-img4.png"/>
+
+
+					</div></li>
+			</ul>
+
+
+		</div>
+		<div class="box testimonial">
+
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque auctor massa ac justo volutpat, sit amet condimentum </p>
+
+
+
+		</div>
+		<div class="box ourProcess">
+			<h2>Interdisciplinary skills</h2>
+			<p>At GPR Dehler we pull together experts from different fields to change or organisations </p>
+			<ul>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills1.png"/>
+					Cognitive Science
+				<p>Lorem ipsum dolor sit consectetur adipiscing
+					commodo varius neque</p>
+				</li>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills2.png"/>
+					Consulting
+
+					<p>Lorem ipsum dolor sit consectetur adipiscing
+						commodo varius neque</p>
+				</li>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills3.png"/>
+					Economics
+
+								<p>Lorem ipsum dolor sit consectetur adipiscing
+					commodo varius neque</p>
+				</li>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills4.png"/>
+					Computer Science
+
+								<p>Lorem ipsum dolor sit consectetur adipiscing
+					commodo varius neque</p>
+				</li>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills5.png"/>
+					Management Theory
+
+								<p>Lorem ipsum dolor sit consectetur adipiscing
+					commodo varius neque</p>
+				</li>
+				<li>
+					<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/home/skills/skills6.png"/>
+					Behavioural Science
+
+								<p>Lorem ipsum dolor sit consectetur adipiscing
+					commodo varius neque</p>
+				</li>
+
+			</ul>
 		</div>
 
 
