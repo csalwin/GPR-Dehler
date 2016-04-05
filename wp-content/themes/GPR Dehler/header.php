4af3b5e3f332fa20faf113d737a88aa6c50412c7
Chris Alwin <chris@somethingbig.co.uk>
2016-04-05 16:08:36 +0100

    add captcha to site

--- wp-content/themes/GPR Dehler/header.php
+++ wp-content/themes/GPR Dehler/header.php
@@ -29,6 +29,7 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
 	<![endif]-->
+	<script src='https://www.google.com/recaptcha/api.js'></script>
 	<?php wp_head(); ?>
 </head>
 
