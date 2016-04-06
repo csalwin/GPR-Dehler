26b00574901931989b7d14a8b75d9a30705a5e7e
Chris Alwin <chris@somethingbig.co.uk>
2016-04-06 11:42:25 +0100

    fix loading two images in the header

--- wp-content/themes/GPR Dehler/includes/headerimagetitle.php
+++ wp-content/themes/GPR Dehler/includes/headerimagetitle.php
@@ -5,15 +5,13 @@
         while (have_posts()) : the_post();
             if (has_post_thumbnail($post->ID)) { ?>
                 <?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
-                <img class="headerImg hidden-xs hidden-sm" src="<?php echo $image[0] ?>" data-stellar-ratio="0.8"
-                     data-stellar-vertical-offset="-150"/>
-                <img class="headerImg hidden-md hidden-lg" src="<?php echo $image[0] ?>"/>
+                <img class="headerImg" src="<?php echo $image[0] ?>"/>
                 <?php
             } else {
                 ?>
                 <img class="headerImg"
                      src="<?php echo esc_url(get_template_directory_uri()); ?>/images/industries/header.png"
-                     alt="headerImage" data-stellar-ratio="0.8"/>
+                     alt="headerImage"/>
             <?php } ?>
             <div class="width970"><h1><?php echo get_the_title(); ?></h1></div>
         <?php endwhile;
@@ -22,7 +20,7 @@
 
         <img class="headerImg"
              src="<?php echo esc_url(get_template_directory_uri()); ?>/images/industries/header.png"
-             alt="headerImage" data-stellar-ratio="0.8"/>
+             alt="headerImage"/>
 
         <?php
     }
